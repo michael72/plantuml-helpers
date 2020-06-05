4d5063793c5fad29cf66ca2b23a23cd0bfbf789d
Michael Schulte <michaelschu@miltenyibiotec.de>
2020-06-05 15:48:47 +0200

    formatting done

--- test/reformat.spec.ts
+++ test/reformat.spec.ts
@@ -4,246 +4,246 @@ should();
 
 describe("Reformat", () => {
 
-	it("should not crash when sorting an empty string", () => {
-		expect(reformat.autoFormatTxt("")).to.be.equal("");
-	});
+    it("should not crash when sorting an empty string", () => {
+        expect(reformat.autoFormatTxt("")).to.be.equal("");
+    });
 
-	it("should not crash when no sortable content", () => {
-		const chk = "Hello World!";
-		expect(reformat.autoFormatTxt(chk)).to.be.equal(chk);
-	});
+    it("should not crash when no sortable content", () => {
+        const chk = "Hello World!";
+        expect(reformat.autoFormatTxt(chk)).to.be.equal(chk);
+    });
 
-	it("should order depending lines from -> to", () => {
-		const lines = ["[B] -> [C]", "[A] -> [B]"];
-		const orig = lines.join("\n");
-		const expected = lines.reverse().join("\n");
-		expect(reformat.autoFormatTxt(orig)).to.be.equal(expected);
-	});
+    it("should order depending lines from -> to", () => {
+        const lines = ["[B] -> [C]", "[A] -> [B]"];
+        const orig = lines.join("\n");
+        const expected = lines.reverse().join("\n");
+        expect(reformat.autoFormatTxt(orig)).to.be.equal(expected);
+    });
 
-	it("should leave the order intact when there is nothing to sort", () => {
-		const lines = ["[C] -> [B]", "[A] -> [B]"];
-		const orig = lines.join("\n");
-		expect(reformat.autoFormatTxt(orig)).to.be.equal(orig);
-	});
+    it("should leave the order intact when there is nothing to sort", () => {
+        const lines = ["[C] -> [B]", "[A] -> [B]"];
+        const orig = lines.join("\n");
+        expect(reformat.autoFormatTxt(orig)).to.be.equal(orig);
+    });
 
-	it("should draw inheritance vertical up, others horizontal", () => {
-		const original =
-			"[Src] o-> [MyDerived2]\n" +
-			"[Derived] o-> Db\n" +
-			"[Foo] -|> IFoo\n" +
-			"[Foo] o-> [Proxy]\n" +
-			"[MyDerived2] -|> [Base]\n" +
-			"[Proxy] o-> [MyDerived1]\n" +
-			"IFoo o-> IBase\n" +
-			"IBase <-o [Base]\n" + // subtle change in direction here
-			"[MyDerived1] -|> [Base]\n" +
-			"[Derived] -|> IBase\n";
+    it("should draw inheritance vertical up, others horizontal", () => {
+        const original =
+            "[Src] o-> [MyDerived2]\n" +
+            "[Derived] o-> Db\n" +
+            "[Foo] -|> IFoo\n" +
+            "[Foo] o-> [Proxy]\n" +
+            "[MyDerived2] -|> [Base]\n" +
+            "[Proxy] o-> [MyDerived1]\n" +
+            "IFoo o-> IBase\n" +
+            "IBase <-o [Base]\n" + // subtle change in direction here
+            "[MyDerived1] -|> [Base]\n" +
+            "[Derived] -|> IBase\n";
 
-		const expected =
-			"IFoo o-> IBase\n" +
-			"IFoo <|-- [Foo]\n" +
-			"IBase <-o [Base]\n" +
-			"IBase <|-- [Derived]\n" +
-			"[Foo] o-> [Proxy]\n" +
-			"[Base] <|-- [MyDerived2]\n" +
-			"[Base] <|-- [MyDerived1]\n" +
-			"[Derived] o-> Db\n" +
-			"[Proxy] o-> [MyDerived1]\n" +
-			"[Src] o-> [MyDerived2]\n";
+        const expected =
+            "IFoo o-> IBase\n" +
+            "IFoo <|-- [Foo]\n" +
+            "IBase <-o [Base]\n" +
+            "IBase <|-- [Derived]\n" +
+            "[Foo] o-> [Proxy]\n" +
+            "[Base] <|-- [MyDerived2]\n" +
+            "[Base] <|-- [MyDerived1]\n" +
+            "[Derived] o-> Db\n" +
+            "[Proxy] o-> [MyDerived1]\n" +
+            "[Src] o-> [MyDerived2]\n";
 
-		const actual = reformat.autoFormatTxt(original);
-		expect(actual).to.be.equal(expected);
+        const actual = reformat.autoFormatTxt(original);
+        expect(actual).to.be.equal(expected);
 
-	});
+    });
 
-	it("should auto format inheritance vertical up", () => {
-		reformat.autoFormatTxt("A -|> B ").should.equal("B <|-- A ");
-	});
+    it("should auto format inheritance vertical up", () => {
+        reformat.autoFormatTxt("A -|> B ").should.equal("B <|-- A ");
+    });
 
-	it("should leave inheritance down when it is down", () => {
-		reformat.autoFormatTxt("A --|> B").should.equal("A --|> B");
-	});
+    it("should leave inheritance down when it is down", () => {
+        reformat.autoFormatTxt("A --|> B").should.equal("A --|> B");
+    });
 
-	it("should auto format composition horizontal right", () => {
-		const original = "A *--> B\n" +
-			"D o--> A";
-		const expected = "D o-> A\n" +
-			"A *-> B";
-		const actual = reformat.autoFormatTxt(original);
-		actual.should.equal(expected);
-	});
+    it("should auto format composition horizontal right", () => {
+        const original = "A *--> B\n" +
+            "D o--> A";
+        const expected = "D o-> A\n" +
+            "A *-> B";
+        const actual = reformat.autoFormatTxt(original);
+        actual.should.equal(expected);
+    });
 
-	it("should leave notes at their positions", () => {
-		const original = "A *--> B\n" +
-			"note right: this is A\n" +
-			"note left: and another note\n" +
-			"D *-> A\n";
-		const expected = "D *-> A\n" +
-			"A *-> B\n" +
-			"note right: this is A\n" +
-			"note left: and another note\n";
-		const actual = reformat.autoFormatTxt(original);
-		actual.should.equal(expected);
-	});
+    it("should leave notes at their positions", () => {
+        const original = "A *--> B\n" +
+            "note right: this is A\n" +
+            "note left: and another note\n" +
+            "D *-> A\n";
+        const expected = "D *-> A\n" +
+            "A *-> B\n" +
+            "note right: this is A\n" +
+            "note left: and another note\n";
+        const actual = reformat.autoFormatTxt(original);
+        actual.should.equal(expected);
+    });
 
-	it("should leave forward declarations at the beginning", () => {
-		const original = "[A] as compA\n" +
-			"note right: this is compA\n" +
-			"A *--> B\n" +
-			"D *-> A\n";
-		const expected = "component A as compA\n" +
-			"note right: this is compA\n" +
-			"D *-> [compA]\n" +
-			"[compA] *-> B\n";
+    it("should leave forward declarations at the beginning", () => {
+        const original = "[A] as compA\n" +
+            "note right: this is compA\n" +
+            "A *--> B\n" +
+            "D *-> A\n";
+        const expected = "component A as compA\n" +
+            "note right: this is compA\n" +
+            "D *-> [compA]\n" +
+            "[compA] *-> B\n";
 
-		const actual = reformat.autoFormatTxt(original);
-		actual.should.equal(expected);
-	});
+        const actual = reformat.autoFormatTxt(original);
+        actual.should.equal(expected);
+    });
 
-	it("should add brackets on components where applicable", () => {
-		const original = "[A] as compA\n" +
-			"component B\n" +
-			"interface IC\n" +
-			"A -> B\n" +
-			"compA -> [IC]\n";
-		const expected = "component A as compA\n" +
-			"component B\n" +
-			"interface IC\n" +
-			"[compA] -> [B]\n" +
-			"[compA] -> IC\n";
-		const actual = reformat.autoFormatTxt(original);
-		actual.should.equal(expected);
-	});
+    it("should add brackets on components where applicable", () => {
+        const original = "[A] as compA\n" +
+            "component B\n" +
+            "interface IC\n" +
+            "A -> B\n" +
+            "compA -> [IC]\n";
+        const expected = "component A as compA\n" +
+            "component B\n" +
+            "interface IC\n" +
+            "[compA] -> [B]\n" +
+            "[compA] -> IC\n";
+        const actual = reformat.autoFormatTxt(original);
+        actual.should.equal(expected);
+    });
 
-	it("should reverse sort packages depending on content dependencencies", () => {
-		const original = "package b {\n" +
-			"  component B\n" +
-			"}\n" +
-			"package c {\n" +
-			"  component C\n" +
-			"}\n" +
-			"package a {\n" +
-			"  component A\n" +
-			"}\n" +
-			"[B] -> [C]\n" +
-			"[A] -> [B]\n";
-		const expected = "package c {\n" +
-			"  component C\n" +
-			"}\n" +
-			"package b {\n" +
-			"  component B\n" +
-			"}\n" +
-			"package a {\n" +
-			"  component A\n" +
-			"}\n" +
-			"[A] -> [B]\n" +
-			"[B] -> [C]\n";
-		const actual = reformat.autoFormatTxt(original);
-		expect(actual).to.be.equal(expected);
-	});
+    it("should reverse sort packages depending on content dependencencies", () => {
+        const original = "package b {\n" +
+            "  component B\n" +
+            "}\n" +
+            "package c {\n" +
+            "  component C\n" +
+            "}\n" +
+            "package a {\n" +
+            "  component A\n" +
+            "}\n" +
+            "[B] -> [C]\n" +
+            "[A] -> [B]\n";
+        const expected = "package c {\n" +
+            "  component C\n" +
+            "}\n" +
+            "package b {\n" +
+            "  component B\n" +
+            "}\n" +
+            "package a {\n" +
+            "  component A\n" +
+            "}\n" +
+            "[A] -> [B]\n" +
+            "[B] -> [C]\n";
+        const actual = reformat.autoFormatTxt(original);
+        expect(actual).to.be.equal(expected);
+    });
 
-	it("should re-arrange the package structure", () => {
-		const original = "package foo {\n" +
-			"IA <|-- [A]\n" +
-			"package inner {\n" +
-			"package ii2 {\n" +
-			"interface IC as InterC\n" +
-			"}\n" +
-			"package ii1 {\n" +
-			"component C\n" +
-			"}\n" +
-			"IC <|-- C\n" +
-			"}\n" +
-			"package inner2 {\n" +
-			"interface D\n" +
-			"}\n" +
-			"}\n" +
-			"package bar {\n" +
-			"interface B\n" +
-			"}\n" +
-			"A o-> B\n" +
-			"A *-> C\n" +
-			"D <|-- C\n" +
-			"[E] o-> F\n";
-		const expected = "package bar {\n" +
-			"  interface B\n" +
-			"}\n" +
-			"package foo {\n" +
-			"  component A\n" +
-			"  interface IA\n" +
-			"  package inner2 {\n" +
-			"    interface D\n" +
-			"  }\n" +
-			"  package inner {\n" +
-			"    package ii2 {\n" +
-			"      interface IC as InterC\n" +
-			"    }\n" +
-			"    package ii1 {\n" +
-			"      component C\n" +
-			"    }\n" +
-			"  }\n" +
-			"}\n" +
-			"IA <|-- [A]\n" +
-			"[A] o-> B\n" +
-			"[A] *-> [C]\n" +
-			"InterC <|-- [C]\n" +
-			"D <|-- [C]\n" +
-			"[E] o-> F\n";
-		const actual = reformat.autoFormatTxt(original);
-		actual.should.equal(expected);
-	});
+    it("should re-arrange the package structure", () => {
+        const original = "package foo {\n" +
+            "IA <|-- [A]\n" +
+            "package inner {\n" +
+            "package ii2 {\n" +
+            "interface IC as InterC\n" +
+            "}\n" +
+            "package ii1 {\n" +
+            "component C\n" +
+            "}\n" +
+            "IC <|-- C\n" +
+            "}\n" +
+            "package inner2 {\n" +
+            "interface D\n" +
+            "}\n" +
+            "}\n" +
+            "package bar {\n" +
+            "interface B\n" +
+            "}\n" +
+            "A o-> B\n" +
+            "A *-> C\n" +
+            "D <|-- C\n" +
+            "[E] o-> F\n";
+        const expected = "package bar {\n" +
+            "  interface B\n" +
+            "}\n" +
+            "package foo {\n" +
+            "  component A\n" +
+            "  interface IA\n" +
+            "  package inner2 {\n" +
+            "    interface D\n" +
+            "  }\n" +
+            "  package inner {\n" +
+            "    package ii2 {\n" +
+            "      interface IC as InterC\n" +
+            "    }\n" +
+            "    package ii1 {\n" +
+            "      component C\n" +
+            "    }\n" +
+            "  }\n" +
+            "}\n" +
+            "IA <|-- [A]\n" +
+            "[A] o-> B\n" +
+            "[A] *-> [C]\n" +
+            "InterC <|-- [C]\n" +
+            "D <|-- [C]\n" +
+            "[E] o-> F\n";
+        const actual = reformat.autoFormatTxt(original);
+        actual.should.equal(expected);
+    });
 
-	it("should re-format nested components", () => {
-		const original = "class Foo <<bla>> #red-green {\n" +
-			"  A -> B\n" +
-			"}\n" +
-			"A -> C\n";
-		const expected = "class Foo <<bla>> #red-green {\n" +
-			"  class B\n" +
-			"  class A\n" +
-			"}\n" +
-			"A -> B\n" +
-			"A -> C\n";
-		const actual = reformat.autoFormatTxt(original);
-		actual.should.equal(expected);
-	});
+    it("should re-format nested components", () => {
+        const original = "class Foo <<bla>> #red-green {\n" +
+            "  A -> B\n" +
+            "}\n" +
+            "A -> C\n";
+        const expected = "class Foo <<bla>> #red-green {\n" +
+            "  class B\n" +
+            "  class A\n" +
+            "}\n" +
+            "A -> B\n" +
+            "A -> C\n";
+        const actual = reformat.autoFormatTxt(original);
+        actual.should.equal(expected);
+    });
 
-	it("should reformat nested with global classes", () => {
-		const original = "class BaseClass\n" +
-			"package net.dummy #DDDDDD {\n" +
-			"  BaseClass <|-- DPerson\n" +
-			"}\n" +
-			'package "net foo" {\n' +
-			"  BaseClass <|-- Person\n" +
-			"}\n" +
-			'"net foo" -- net.dummy\n';
-		const expected = 'package "net foo" {\n' +
-			"  class Person\n" +
-			"}\n" +
-			"package net.dummy #DDDDDD {\n" +
-			"  class DPerson\n" +
-			"}\n" +
-			"class BaseClass\n" +
-			"BaseClass <|-- DPerson\n" +
-			"BaseClass <|-- Person\n" +
-			'"net foo" -- net.dummy\n';
-		const actual = reformat.autoFormatTxt(original);
-		actual.should.equal(expected);
-	});
+    it("should reformat nested with global classes", () => {
+        const original = "class BaseClass\n" +
+            "package net.dummy #DDDDDD {\n" +
+            "  BaseClass <|-- DPerson\n" +
+            "}\n" +
+            'package "net foo" {\n' +
+            "  BaseClass <|-- Person\n" +
+            "}\n" +
+            '"net foo" -- net.dummy\n';
+        const expected = 'package "net foo" {\n' +
+            "  class Person\n" +
+            "}\n" +
+            "package net.dummy #DDDDDD {\n" +
+            "  class DPerson\n" +
+            "}\n" +
+            "class BaseClass\n" +
+            "BaseClass <|-- DPerson\n" +
+            "BaseClass <|-- Person\n" +
+            '"net foo" -- net.dummy\n';
+        const actual = reformat.autoFormatTxt(original);
+        actual.should.equal(expected);
+    });
 
-	it("should reformat nested empty components", () => {
-		const original = "package foo {\n" +
-			"  component A {\n" +
-			"  }\n" +
-			"}\n" +
-			"A --|> IA\n";
-		const expected = "package foo {\n" +
-		"  component A {\n" +
-		"  }\n" +
-		"}\n" +
-		"[A] --|> IA\n";
-		const actual = reformat.autoFormatTxt(original);
-		actual.should.equal(expected);
-	})
+    it("should reformat nested empty components", () => {
+        const original = "package foo {\n" +
+            "  component A {\n" +
+            "  }\n" +
+            "}\n" +
+            "A --|> IA\n";
+        const expected = "package foo {\n" +
+            "  component A {\n" +
+            "  }\n" +
+            "}\n" +
+            "[A] --|> IA\n";
+        const actual = reformat.autoFormatTxt(original);
+        actual.should.equal(expected);
+    })
 
 });
--- test/uml/component.spec.ts
+++ test/uml/component.spec.ts
@@ -7,151 +7,151 @@ should();
 
 describe("Component", () => {
     describe("Component class", () => {
-    it("should parse a package", () => {
-        const original =
-            'package "fun" {\n' +
-            "  component A\n" +
-            "  note right: this is A\n" +
-            "  A *--> B\n" +
-            "  D *-> A\n" +
-            "}\n";
-        const parsed = Component.fromString(original);
-        parsed.name!.should.equal("fun");
-        parsed.type!.should.equal("package");
-        parsed.content.length.should.equal(4);
-        parsed.toString().should.equal(original);
+        it("should parse a package", () => {
+            const original =
+                'package "fun" {\n' +
+                "  component A\n" +
+                "  note right: this is A\n" +
+                "  A *--> B\n" +
+                "  D *-> A\n" +
+                "}\n";
+            const parsed = Component.fromString(original);
+            parsed.name!.should.equal("fun");
+            parsed.type!.should.equal("package");
+            parsed.content.length.should.equal(4);
+            parsed.toString().should.equal(original);
+        });
+        it("should parse a package with `}` in next line", () => {
+            const original = ["package bla <<something>>", '{', "[A]", '}'];
+            // { is moved up when converting back to string - hence:
+            const expected = 'package bla <<something>> {\n' +
+                "  component A\n" +
+                "}\n";
+            const parsed = Component.fromString(original);
+            parsed.name!.should.equal("bla");
+            parsed.type!.should.equal("package");
+            parsed.content.length.should.equal(1);
+            parsed.toString().should.equal(expected);
+        });
+        it("should parse an empty package", () => {
+            const original = 'package {\n}\n';
+            const parsed = Component.fromString(original);
+            const actual = parsed.toString();
+
+            actual.should.equal(original);
+        });
+        it("should parse two sibling packages", () => {
+            const original = 'package a {\n' +
+                '  component A\n' +
+                '}\n' +
+                'package b {\n' +
+                '  [A] -> [B]\n' +
+                '}';
+            const expected = original;
+            const parsed = Component.fromString(original);
+            const actual = parsed.toString();
+            actual.should.equal(expected);
+        });
+        it("should parse a nested component", () => {
+            const original = 'package "a" {\n' +
+                ' component A\n' +
+                ' interface IA\n' +
+                ' package "b" {\n' +
+                '   interface IB\n' +
+                '   component B\n' +
+                ' }\n' +
+                ' package "c" {\n' +
+                '   interface IC\n' +
+                '   component C\n' +
+                '  package inner {\n' +
+                '  }\n' +
+                ' }\n' +
+                '\n' +
+                ' IA <|-- A\n' +
+                ' IB <|-- B\n' +
+                ' IC <|-- C\n' +
+                '\n' +
+                ' A o-> IB\n' +
+                ' B o-> IC\n' +
+                '}\n';
+            const parsed = Component.fromString(original);
+            const actual = parsed.toString();
+            const expected = 'package "a" {\n' +
+                '  component A\n' +
+                '  interface IA\n' +
+                '  package "b" {\n' +
+                '    interface IB\n' +
+                '    component B\n' +
+                '  }\n' +
+                '  package "c" {\n' +
+                '    interface IC\n' +
+                '    component C\n' +
+                '    package inner {\n' +
+                '    }\n' +
+                '  }\n' +
+                '  IA <|-- A\n' +
+                '  IB <|-- B\n' +
+                '  IC <|-- C\n' +
+                '  A o-> IB\n' +
+                '  B o-> IC\n' +
+                '}\n';
+            actual.should.equal(expected);
+        });
     });
-    it("should parse a package with `}` in next line", () => {
-        const original = ["package bla <<something>>", '{', "[A]", '}'];
-        // { is moved up when converting back to string - hence:
-        const expected = 'package bla <<something>> {\n' +
-            "  component A\n" +
-            "}\n";
-        const parsed = Component.fromString(original);
-        parsed.name!.should.equal("bla");
-        parsed.type!.should.equal("package");
-        parsed.content.length.should.equal(1);
-        parsed.toString().should.equal(expected);
+    describe("Definition class", () => {
+        it("should parse component definitions", () => {
+
+            const c1 = Definition.fromString("[First component]")!;
+            c1.type.should.equal("component");
+            c1.name.should.equal("First component");
+            equal(c1.alias, undefined);
+
+            const c2 = Definition.fromString("[Another component] as Comp2")!;
+            c2.type.should.equal("component");
+            c2.name.should.equal("Another component");
+            c2.alias!.should.equal("Comp2");
+
+            const c3 = Definition.fromString("component Comp3")!;
+            c3.type.should.equal("component");
+            c3.name.should.equal("Comp3");
+            equal(c3.alias, undefined);
+
+            const c4 = Definition.fromString("component [Last\tcomponent] as Comp4")!;
+            c4.type.should.equal("component");
+            c4.name.should.equal("Last\tcomponent");
+            c4.alias!.should.equal("Comp4");
+        });
+
+        it("should parse an interface definition", () => {
+
+            const c1 = Definition.fromString('() "First Interface"')!;
+            c1.type.should.equal("interface");
+            c1.name.should.equal("First Interface");
+            equal(c1.alias, undefined);
+
+            const c2 = Definition.fromString('() "Another interface" as Interf2')!;
+            c2.type.should.equal("interface");
+            c2.name.should.equal("Another interface");
+            c2.alias!.should.equal("Interf2");
+
+            const c3 = Definition.fromString('interface Interf3')!;
+            c3.type.should.equal("interface");
+            c3.name.should.equal("Interf3");
+            equal(c3.alias, undefined);
+
+            const c4 = Definition.fromString('interface "Last\\ninterface" as Interf4')!;
+            c4.type.should.equal("interface");
+            c4.name.should.equal("Last\\ninterface");
+            c4.alias!.should.equal("Interf4");
+
+        });
+
+        it("should not parse an invalid definition", () => {
+            ["a b c", "ABC", 'package "blub"', "interf as bla"].forEach((s: string) => {
+                const def = Definition.fromString(s);
+                equal(def, undefined);
+            });
+        });
+
     });
-    it("should parse an empty package", () => {
-        const original = 'package {\n}\n';
-        const parsed = Component.fromString(original);
-        const actual = parsed.toString();
-
-        actual.should.equal(original);
-    });
-    it("should parse two sibling packages", () => {
-        const original = 'package a {\n' + 
-            '  component A\n' +
-            '}\n' +
-            'package b {\n' +
-            '  [A] -> [B]\n' +
-            '}';
-        const expected = original;
-        const parsed = Component.fromString(original);
-        const actual = parsed.toString();
-        actual.should.equal(expected);
-    });
-    it("should parse a nested component", () => {
-        const original = 'package "a" {\n' +
-            ' component A\n' +
-            ' interface IA\n' +
-            ' package "b" {\n' +
-            '   interface IB\n' +
-            '   component B\n' +
-            ' }\n' +
-            ' package "c" {\n' +
-            '   interface IC\n' +
-            '   component C\n' +
-            '  package inner {\n' +
-            '  }\n' +
-            ' }\n' +
-            '\n' +
-            ' IA <|-- A\n' +
-            ' IB <|-- B\n' +
-            ' IC <|-- C\n' +
-            '\n' +
-            ' A o-> IB\n' +
-            ' B o-> IC\n' +
-            '}\n';
-        const parsed = Component.fromString(original);
-        const actual = parsed.toString();
-        const expected = 'package "a" {\n' +
-            '  component A\n' +
-            '  interface IA\n' +
-            '  package "b" {\n' +
-            '    interface IB\n' +
-            '    component B\n' +
-            '  }\n' +
-            '  package "c" {\n' +
-            '    interface IC\n' +
-            '    component C\n' +
-            '    package inner {\n' +
-            '    }\n' +
-            '  }\n' +
-            '  IA <|-- A\n' +
-            '  IB <|-- B\n' +
-            '  IC <|-- C\n' +
-            '  A o-> IB\n' +
-            '  B o-> IC\n' +
-            '}\n';
-        actual.should.equal(expected);
-    });
-    });
-describe("Definition class", () => {
- it("should parse component definitions", () => {
-
-    const c1 = Definition.fromString("[First component]")!;
-    c1.type.should.equal("component");
-    c1.name.should.equal("First component");
-    equal(c1.alias, undefined);
-
-    const c2 = Definition.fromString("[Another component] as Comp2")!;
-    c2.type.should.equal("component");
-    c2.name.should.equal("Another component");
-    c2.alias!.should.equal("Comp2");
-
-    const c3 = Definition.fromString("component Comp3")!;
-    c3.type.should.equal("component");
-    c3.name.should.equal("Comp3");
-    equal(c3.alias, undefined);
-
-    const c4 = Definition.fromString("component [Last\tcomponent] as Comp4")!;
-    c4.type.should.equal("component");
-    c4.name.should.equal("Last\tcomponent");
-    c4.alias!.should.equal("Comp4");
- });
-
- it ("should parse an interface definition", () => {
-
-    const c1 = Definition.fromString('() "First Interface"')!;
-    c1.type.should.equal("interface");
-    c1.name.should.equal("First Interface");
-    equal(c1.alias, undefined);
-
-    const c2 = Definition.fromString('() "Another interface" as Interf2')!;
-    c2.type.should.equal("interface");
-    c2.name.should.equal("Another interface");
-    c2.alias!.should.equal("Interf2");
-
-    const c3 = Definition.fromString('interface Interf3')!;
-    c3.type.should.equal("interface");
-    c3.name.should.equal("Interf3");
-    equal(c3.alias, undefined);
-
-    const c4 = Definition.fromString('interface "Last\\ninterface" as Interf4')!;
-    c4.type.should.equal("interface");
-    c4.name.should.equal("Last\\ninterface");
-    c4.alias!.should.equal("Interf4");
-    
- });
-
- it ("should not parse an invalid definition", () => {
-     ["a b c", "ABC", 'package "blub"', "interf as bla"].forEach((s: string) => {
-        const def = Definition.fromString(s);
-        equal(def, undefined); 
-     });
- });
-
-});
 });
